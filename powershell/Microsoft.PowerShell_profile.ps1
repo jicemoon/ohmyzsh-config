75f49cab6e5575b6aca543186779b94910f7ce99
jicemoon <jicemoon@outlook.com>
2022-03-22 23:54:45 +0400

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 ﻿chcp 65001   ##这个是c:/user/document/powershell/Microsoft.PowerShell_profile.ps1
 Set-PSReadlineOption -EditMode Emacs
+
+# 设置窗口标题
+$Host.UI.RawUI.WindowTitle = $((Get-Location | Get-Item ).Name)
+
 function which($name) { Get-Command $name | Select-Object Definition }
 function rmrf($item) { Remove-Item $item -Recurse -Force }
 function touch($item) { New-Item $item -ItemType File }
